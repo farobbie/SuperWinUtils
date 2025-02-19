712f5db792f667bc50147a37a235ceec8fe84164
farrobbie <farobi91@gmail.com>
2025-02-19 15:40:44 +0100

    Add BaseViewModel

---
+++ SuperWinUtils/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SuperWinUtils.ViewModels;
+
+public partial class BaseViewModel : ObservableRecipient
+{
+    public BaseViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    public partial string? Title { get; set; }
+
+
+    [ObservableProperty]
+    public partial bool IsBusy { get; set; }
+
+    protected bool IsNotBusy => !IsBusy;
+}
