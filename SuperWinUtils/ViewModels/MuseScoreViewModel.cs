6822b602bf33340ea2875831d96dc10df0641f1a
farrobbie <farobi91@gmail.com>
2025-02-24 11:31:57 +0100

    Add properties in museScoreViewModel

--- SuperWinUtils/ViewModels/MuseScoreViewModel.cs
+++ SuperWinUtils/ViewModels/MuseScoreViewModel.cs
@@ -1,13 +1,35 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SuperWinUtils.Contracts.Services;
 using SuperWinUtils.Helpers;
 
 namespace SuperWinUtils.ViewModels;
 
 public partial class MuseScoreViewModel : BaseViewModel
 {
-    public MuseScoreViewModel()
+    private readonly ILocalSettingsService _localSettingsService;
+
+    private const string _settingsKeySourceFileUrl = "MuseScore_SourceFileUrl";
+    private const string _settingsKeySourceFilePath = "MuseScore_SourceFilePath";
+    private const string _settingsKeyDestinationFilePath = "MuseScore_DestinationFilePath";
+
+    private readonly string _defaultSourceFileUrl = "MuseScore_SourceFileUrl_Default".GetLocalized() ?? "";
+    private readonly string _defaultSourceFilePath = "MuseScore_SourceFilePath_Default".GetLocalized() ?? "";
+    private readonly string _defaultDestinationFilePath = "MuseScore_DestinationFilePath_Default".GetLocalized() ?? "";
+
+    [ObservableProperty]
+    public partial string SourceFileUrl { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial string SourceFilePath { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial string DestinationFilePath { get; set; } = string.Empty;
+
+    public MuseScoreViewModel(ILocalSettingsService localSettingsService)
     {
         Title = "MuseScore";
+        _localSettingsService = localSettingsService;
     }
 
 
