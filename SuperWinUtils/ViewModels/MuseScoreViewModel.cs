eea9c3d765490c22a8c0b497c48bbced4616a2dd
farrobbie <farobi91@gmail.com>
2025-02-25 23:36:25 +0100

    Add setting-widgets and save-, reset-setting-methods for musescore

--- SuperWinUtils/ViewModels/MuseScoreViewModel.cs
+++ SuperWinUtils/ViewModels/MuseScoreViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
 using SuperWinUtils.Contracts.Services;
 using SuperWinUtils.Helpers;
 
@@ -13,23 +14,43 @@ public partial class MuseScoreViewModel : BaseViewModel
     private const string _settingsKeySourceFilePath = "MuseScore_SourceFilePath";
     private const string _settingsKeyDestinationFilePath = "MuseScore_DestinationFilePath";
 
-    private readonly string _defaultSourceFileUrl = "MuseScore_SourceFileUrl_Default".GetLocalized() ?? "";
-    private readonly string _defaultSourceFilePath = "MuseScore_SourceFilePath_Default".GetLocalized() ?? "";
-    private readonly string _defaultDestinationFilePath = "MuseScore_DestinationFilePath_Default".GetLocalized() ?? "";
+    private readonly string _defaultSourceFileUrl = "MuseScore_SourceFileUrl_Default".GetLocalized() ?? "SourceFileUrl";
+    private readonly string _defaultSourceFilePath = "MuseScore_SourceFilePath_Default".GetLocalized() ?? "SourceFilePath";
+    private readonly string _defaultDestinationFilePath = "MuseScore_DestinationFilePath_Default".GetLocalized() ?? "DestinationFilePath";
 
     [ObservableProperty]
-    public partial string SourceFileUrl { get; set; } = string.Empty;
+    public partial string SourceFileUrl { get; set; }
 
     [ObservableProperty]
-    public partial string SourceFilePath { get; set; } = string.Empty;
+    public partial string SourceFilePath { get; set; }
 
     [ObservableProperty]
-    public partial string DestinationFilePath { get; set; } = string.Empty;
+    public partial string DestinationFilePath { get; set; }
 
     public MuseScoreViewModel(ILocalSettingsService localSettingsService)
     {
         Title = "MuseScore";
         _localSettingsService = localSettingsService;
+
+        SourceFileUrl = _defaultSourceFileUrl;
+        SourceFilePath = _defaultSourceFilePath;
+        DestinationFilePath = _defaultDestinationFilePath;
+        
+        _ = InitializeAsync();
+    }
+
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            SourceFileUrl = await _localSettingsService.ReadSettingAsync<string>(_settingsKeySourceFileUrl) ?? _defaultSourceFileUrl;
+            SourceFilePath = await _localSettingsService.ReadSettingAsync<string>(_settingsKeySourceFilePath) ?? _defaultSourceFilePath;
+            DestinationFilePath = await _localSettingsService.ReadSettingAsync<string>(_settingsKeyDestinationFilePath) ?? _defaultDestinationFilePath;
+        }
+        catch (Exception ex)
+        {
+            await _dialogService.ShowAlertDialogAsync(ex.Message);
+        }
     }
 
 
@@ -61,4 +82,47 @@ public partial class MuseScoreViewModel : BaseViewModel
         }
     }
 
+
+    [RelayCommand]
+    private async Task SaveSettingAsync(string setting)
+    {
+        var (key, settingVar, message) = setting switch
+        {
+            "SourceFileUrl" => (_settingsKeySourceFileUrl, SourceFileUrl, "Save SourceFileUrl"),
+            "SourceFilePath" => (_settingsKeySourceFilePath, SourceFilePath, "Save SourceFilePath"),
+            "DestinationFilePath" => (_settingsKeyDestinationFilePath, DestinationFilePath, "Save DestinationFilePath"),
+            _ => (string.Empty, string.Empty, "Wrong Textbox!")
+        };
+
+        if (key != null)
+        {
+            await _localSettingsService.SaveSettingAsync(key, settingVar);
+        }
+
+        await ReportStatus(message);
+    }
+
+
+    [RelayCommand]
+    private async Task ResetSettingAsync(string setting)
+    {
+        switch (setting)
+        {
+            case "SourceFileUrl":
+                SourceFileUrl = _defaultSourceFileUrl;
+                await _localSettingsService.SaveSettingAsync(_settingsKeySourceFileUrl, SourceFileUrl);
+                break;
+            case "SourceFilePath":
+                SourceFilePath = _defaultSourceFilePath;
+                await _localSettingsService.SaveSettingAsync(_settingsKeySourceFilePath, SourceFilePath);
+                break;
+            case "DestinationFilePath":
+                DestinationFilePath = _defaultDestinationFilePath;
+                await _localSettingsService.SaveSettingAsync(_settingsKeyDestinationFilePath, DestinationFilePath);
+                break;
+            default:
+                await ReportStatus($"Wrong Textbox! {setting}");
+                return;
+        }
+    }
 }
