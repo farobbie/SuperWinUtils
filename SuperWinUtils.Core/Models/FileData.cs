e19b2e3e200e419144b942ff2ace7277b465e39c
farrobbie <farobi91@gmail.com>
2025-02-26 14:39:08 +0100

    Add FileData

---
+++ SuperWinUtils.Core/Models/FileData.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperWinUtils.Core.Models;
+public class FileData
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public DateTime Date { get; set; }
+    public byte[] Content { get; set; }
+}
