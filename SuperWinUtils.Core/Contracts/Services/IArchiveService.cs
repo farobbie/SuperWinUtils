a998c4c60bb1b29b45c9b44c6491466a2efbf16b
farrobbie <farobi91@gmail.com>
2025-02-26 14:46:26 +0100

    Add ExtractFileToAsync to ArchiveService

--- SuperWinUtils.Core/Contracts/Services/IArchiveService.cs
+++ SuperWinUtils.Core/Contracts/Services/IArchiveService.cs
@@ -1,4 +1,5 @@
 ﻿namespace SuperWinUtils.Core.Contracts.Services;
 public interface IArchiveService
 {
+    Task ExtractFileToAsync(string sourceFilePath, string destinationPath, IProgress<double> progress, CancellationToken cancellationToken);
 }
--- SuperWinUtils.Core/Services/ArchiveService.cs
+++ SuperWinUtils.Core/Services/ArchiveService.cs
@@ -1,7 +1,33 @@
-﻿using SuperWinUtils.Core.Contracts.Services;
+﻿using SharpCompress.Archives;
+using SuperWinUtils.Core.Contracts.Services;
 
 namespace SuperWinUtils.Core.Services;
 public class ArchiveService : IArchiveService
 {
+    public async Task ExtractFileToAsync(string sourceFilePath, string destinationPath, IProgress<double> progress, CancellationToken cancellationToken)
+    {
+        await Task.Run(() =>
+        {
+            using var archive = ArchiveFactory.Open(sourceFilePath);
+            var totalEntries = archive.Entries.Count();
+            var processedEntries = 0;
+            foreach (var entry  in archive.Entries)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
 
+                if (!entry.IsDirectory)
+                {
+                    entry.WriteToDirectory(destinationPath, new SharpCompress.Common.ExtractionOptions
+                    {
+                        ExtractFullPath = true,
+                        Overwrite = true
+                    });
+                }
+
+                processedEntries++;
+                progress.Report((double)processedEntries / totalEntries);
+            }
+
+        }, cancellationToken);
+    }
 }
