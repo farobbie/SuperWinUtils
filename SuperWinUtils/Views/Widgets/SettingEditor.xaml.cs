a021b64843611a2a975f0a113fad6b38f05d321c
farrobbie <farobi91@gmail.com>
2025-03-10 17:38:40 +0100

    Add images to buttons of settingeditor

--- SuperWinUtils/Views/Widgets/SettingEditor.xaml.cs
+++ SuperWinUtils/Views/Widgets/SettingEditor.xaml.cs
@@ -1,18 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Windows.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System.Windows.Input;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -65,4 +54,20 @@ public sealed partial class SettingEditor : UserControl
     }
     public static readonly DependencyProperty ResetCommandProperty =
         DependencyProperty.Register(nameof(ResetCommand), typeof(ICommand), typeof(SettingEditor), new PropertyMetadata(null));
+    public ImageSource SaveImage
+    {
+        get => (ImageSource)GetValue(SaveImageProperty);
+        set => SetValue(SaveImageProperty, value);
+    }
+    public static readonly DependencyProperty SaveImageProperty =
+        DependencyProperty.Register(nameof(SaveImage), typeof(ImageSource), typeof(SettingEditor), new PropertyMetadata(null));
+
+    public ImageSource ResetImage
+    {
+        get => (ImageSource)GetValue(ResetImageProperty);
+        set => SetValue(ResetImageProperty, value);
+    }
+    public static readonly DependencyProperty ResetImageProperty =
+        DependencyProperty.Register(nameof(ResetImage), typeof(ImageSource), typeof(SettingEditor), new PropertyMetadata(null));
+
 }
