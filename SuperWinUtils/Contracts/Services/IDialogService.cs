e14b3ce8a04acbe0f82119f9a0bbeb9f419acdea
farrobbie <farobi91@gmail.com>
2025-03-25 15:11:45 +0100

    Add filepicker to dialogservice

--- SuperWinUtils/Contracts/Services/IDialogService.cs
+++ SuperWinUtils/Contracts/Services/IDialogService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using Windows.Storage;
 
 namespace SuperWinUtils.Contracts.Services;
 
@@ -8,4 +9,5 @@ public interface IDialogService
     Task ShowWarningDialogAsync(string message);
     Task<ContentDialogResult> ShowOptionMessageDialogAsync(string title, string content, string primaryButtonText, string secondaryButtonText, string closeButtonText = "OK");
     Task<ContentDialogResult> InputStringDialogAsync(string title, string defaultText, string primaryButtonText, string secondaryButtonText, string closeButtonText = "Cancel");
+    Task<IReadOnlyList<StorageFile>> OpenImagesAsync();
 }
